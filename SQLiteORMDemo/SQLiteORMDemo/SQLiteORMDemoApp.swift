61afb89fe5abe53763518bc210538873c9f8b401
Jonas Wolf <jonas.wolf8@me.com>
2025-06-05 07:26:54 +0200

    added example project to the package

---
+++ SQLiteORMDemo/SQLiteORMDemo/SQLiteORMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteORMDemoApp.swift
+//  SQLiteORMDemo
+//
+//  Created by Jonas Wolf on 05.06.25.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteORMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
